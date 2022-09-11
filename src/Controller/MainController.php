a333de2abc19f0c18c9e29aa8cbdde464695cd26
Sébastien Dru <sebdru.fr@gmail.com>
2022-09-11 17:59:54 +0200

    add feature load more button on the main page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Repository\TrickRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use phpDocumentor\Reflection\Types\Array_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,16 +22,26 @@ class MainController extends AbstractController
     {
         //$tricks = $doctrine->getRepository(Trick::class)->getAllTricks();
         return $this->render('main/index.html.twig', [
-            'tricks' => $tricks->findBy([],[],5,0)
+            'tricks' => $tricks->findBy([],['created_at' => 'ASC'],5,0),
+            'offset' => 50
         ]);
     }
+
+    /**
+     * @param TrickRepository $tricks
+     * @param ManagerRegistry $doctrine
+     * @param $offset
+     * @return Response
+     */
     #[Route('/nextTricks/{offset}', name: 'app_loadMore')]
     public function loadMore(TrickRepository $tricks, ManagerRegistry $doctrine, $offset): Response
     {
-        //$tricks = $doctrine->getRepository(Trick::class)->getAllTricks();
-        return $this->render('main/index.html.twig', [
-            'tricks' => $tricks->findBy([],[],5,$offset)
+        $html = $this->render('main/_trickList_partial.html.twig', [
+            'tricks' => $tricks->findBy([],['created_at' => 'ASC'],5,$offset)
         ]);
+
+        return new Response($html);
+
     }
 
 }
